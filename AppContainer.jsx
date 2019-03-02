7d7ec99ec7513a6b89e47302d321ff977cd0eeeb
Roshan <roshannizar8@gmail.com>
2019-03-02 13:43:01 +0530

    react

--- AppContainer.jsx
+++ AppContainer.jsx
@@ -2,6 +2,8 @@
 
 import React, {Component} from 'react';
 
+import AddUser from './AddUser';
+
 import Users from './Users'
 
 export default class AppContainer extends Component{
@@ -14,9 +16,13 @@ export default class AppContainer extends Component{
             }]
         }
     }
+    addUser(user){
+        this.setState(state=>({users: state.users.concat({id: Date.now(), name: user.name})}))
+    }
     render() {
         return <div>
             <h2>Users App</h2>
+            <AddUser addUser={user => this.addUser(user)}/>
             <Users users={this.state.users}/>
         </div>;
     }
